aaa54b38c027a0ca5a518dca6af7116c2c201148
Truong Minh Nhat Quang <nhatquangsin@gmail.com>
2017-06-11 11:17:57 +0700

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">
+                        React Music App
+                    </li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Music</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">About</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by Quang Truong
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+module.exports = Nav;
